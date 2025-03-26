5b95ee59a7e117c7e7e10ce46c94b41a43a03df5
Daivion Brooks <daivionbrooks11@gmail.com>
2025-03-25 21:08:33 -0400

    clear grid button created. rainbowColor function created for random color generation.

--- sketch.js
+++ sketch.js
@@ -1,40 +1,86 @@
 const gridLayout = document.querySelector(".grid.layout");
 const colorChoice = document.querySelector("#color-choice");
+const rows = document.querySelector("#range-rows");
+const cols = document.querySelector("#range-columns");
+const button = document.querySelector("#clear");
+let rowNums;
+let colNums;
+
 let drawing = false;
 
-function createGrid() {
+let rainbowColor = ()=>{
+    const ran1 = Math.floor(Math.random()*256);
+    const ran2 = Math.floor(Math.random()*256);
+    const ran3 = Math.floor(Math.random()*256);
+    return `rgb(${ran1}, ${ran2},${ran3})`;
+}
 
-    // Create 16 rows
-    for(let i = 0; i < 16; i++) {
+
+function createGrid() {
+    clearGrid();
+  
+    
+    if(parseInt(rows.value) > 0 && parseInt(rows.value) <= 100)
+        rowNums = parseInt(rows.value);
+    else
+    rowNums = 16;
+
+    if(parseInt(cols.value) > 0 && parseInt(cols.value) <= 100)
+        colNums = parseInt(cols.value);
+    else
+    colNums = 16;
+    // rows
+    for(let i = 0; i < rowNums; i++) {
         const row = document.createElement("div");
         row.className = "grid-row";
         gridLayout.appendChild(row);
 
         //create cols inrows
-        for(let k = 0; k < 16; k++) {
+        for(let k = 0; k < colNums; k++) {
             const col = document.createElement("div");
             col.className = "grid-col";
             row.appendChild(col);
             
-            // Add event listeners to cols
             col.addEventListener('mousedown', () => {
                 drawing = true;
-                col.style.backgroundColor = colorChoice.value;
+                if (colorChoice.value == "rainbow") {
+                    col.style.backgroundColor = rainbowColor();
+                } else {
+                    col.style.backgroundColor = colorChoice.value;
+                }
             });
             
             col.addEventListener('mouseenter', () => {
                 if (drawing) {
-                    col.style.backgroundColor = colorChoice.value;
+                    if (colorChoice.value == "rainbow") {
+                        col.style.backgroundColor = rainbowColor();
+                    } else {
+                        col.style.backgroundColor = colorChoice.value;
+                    }
                 }
             });
         }
     }
 }
 
-// Stop drawing when mouse is released anywhere on page
+function clearGrid(){
+ gridLayout.textContent = "";
+}
+
+function reloadGrid(){
+    gridLayout.textContent = "";
+    createGrid();
+}
+
+// stop drawing 
 document.addEventListener('mouseup', () => {
     drawing = false;
 });
+
+ 
+button.addEventListener("click", reloadGrid);
+rows.addEventListener("input", createGrid);
+cols.addEventListener("input", createGrid);
 
 createGrid();
 
